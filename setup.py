c317ad9b13de137277a4f26eefc42a7c9461c282
masonschafercodes <mason.schafer1@gmail.com>
2021-02-15 17:03:02 -0500

    initial commit

---
+++ setup.py
@@ -0,0 +1,65 @@
+#!/usr/local/bin/python3 -u
+#
+# The MIT License (MIT)
+#
+# Copyright (c) 2015 by Oliver Ratzesberger
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+
+setup(
+    name='rlb',
+
+    # Versions should comply with PEP440.  For a discussion on single-sourcing
+    # the version across setup.py and the project code, see
+    # https://packaging.python.org/en/latest/single_source_version.html
+    version="0.0.1",  # noqa
+
+    description='React Template Tool',
+    # Author details
+    author="masonschafercodes",
+    author_email="mason.schafer1@gmail.com",
+
+    # Choose your license
+    license="MIT",
+
+    # You can just specify the packages manually here if your project is
+    # simple. Or you can use find_packages().
+    packages=find_packages(),
+
+    # List run-time dependencies here.  These will be installed by pip when
+    # your project is installed. For an analysis of "install_requires" vs pip's
+    # requirements files see:
+    # https://packaging.python.org/en/latest/requirements.html
+    install_requires=[
+        'click>=7.1.2',
+        'colorama>=0.4.4',
+        'gitdb>=4.0.5',
+        'gitpython>=3.1.13',
+        'pyfiglet>=0.8.post1',
+        'smmap>=3.0.5'
+    ],
+
+    entry_points='''
+        [console_scripts]
+        rlb=rlb.__main__:main
+    '''
+)
